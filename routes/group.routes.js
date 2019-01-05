13c97483d82bce1c57cdc335aabe795ab3ae8106
= <=>
2019-01-05 17:30:00 +0700

    lab 2018/01/05 add members into group

--- routes/group.routes.js
+++ routes/group.routes.js
@@ -8,7 +8,9 @@ const router = new Router();
 router.get('/groups', GroupController.getAll);
 router.get('/groups/:id', GroupController.getById);
 router.post('/groups', [validate (validations.create)],  GroupController.create);
-router.put('/groups/:id', [validate (validations.update), middleware.checkId], GroupController.update);
+router.put('/groups/:id', [validate (validations.update)], GroupController.update);
+router.put('/groups/add_members/:id', [validate (validations.update)], GroupController.addMembers);
+router.put('/groups/delete_member/:id', [validate (validations.update)], GroupController.deleteMembers);
 router.delete('/groups/:id', GroupController.delete);
 
 export default router;
--- validation/group.js
+++ validation/group.js
@@ -6,15 +6,17 @@ group.create = {
 		name: Joi.string().required().max(255),
 		lastMessage: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
 		author: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
-		members: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/).required())
+		members: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()),
+		deletedAt: Joi.default(null),
   	}
 };
 group.update = {
     body: {
-		name: Joi.string().required().max(255),
-		lastMessage: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
-		author: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
-		members: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/).required())
+		name: Joi.string().max(255),
+		lastMessage: Joi.string().regex(/^[0-9a-fA-F]{24}$/),
+		author: Joi.string().regex(/^[0-9a-fA-F]{24}$/),
+		members: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()),
+		deletedAt: Joi.empty(Joi.any()).default(null),
   	}
 };
 export default group;
